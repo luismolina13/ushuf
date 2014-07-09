0d52c60ca6fa7957aeb40a24626b342990690480
Luis Molina <luismol@umich.edu>
2014-07-08 23:43:13 -0700

    Added things to youtubeSearch.py

--- youtubeSearch.py
+++ youtubeSearch.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 
 from apiclient.discovery import build
+from optparse import OptionParser
+
 import re
 import difflib
+import json
+import urllib
 
 # Set DEVELOPER_KEY to the "API key" value from the "Access" tab of the
 # Google APIs Console http://code.google.com/apis/console#access
@@ -10,6 +14,7 @@ import difflib
 DEVELOPER_KEY = "AIzaSyBY58aP8fFT9U_G3Q-zQaC3t9B49ZzOHVc"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
+FREEBASE_SEARCH_URL = "https://www.googleapis.com/freebase/v1/search?%s"
 
 youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
   developerKey=DEVELOPER_KEY)
@@ -34,6 +39,33 @@ def getNext(videoId):
 def searchNew():
   log("Searching for a new video")
 
+def get_topic_id(search_query):
+  freebase_params = dict(query="Amazing goals", key=DEVELOPER_KEY)
+  freebase_url = FREEBASE_SEARCH_URL % urllib.urlencode(freebase_params)
+  freebase_response = json.loads(urllib.urlopen(freebase_url).read())
+
+  if len(freebase_response["result"]) == 0:
+    log("No matching terms were found in Freebase.")
+
+  mids = []
+  index = 1
+  print "The following topics were found:"
+  for result in freebase_response["result"]:
+    mids.append(result["mid"])
+    print "  %2d. %s (%s)" % (index, result.get("name", "Unknown"),
+      result.get("notable", {}).get("name", "Unknown"))
+    index += 1
+
+  mid = None
+  while mid is None:
+    index = raw_input("Enter a topic number to find related YouTube %ss: " %
+      "video")
+    try:
+      mid = mids[int(index) - 1]
+    except ValueError:
+      pass
+  return mid
+
 def youtube_search(videoId):
 
   search_response = youtube.search().list(
@@ -49,6 +81,9 @@ def youtube_search(videoId):
   for search_result in search_response.get("items", []):
     search_query += search_result["snippet"]["title"]
     # log(search_query)
+
+  log(search_query)
+  get_topic_id(search_query)
 
   search_response = youtube.search().list(
     q=search_query,
